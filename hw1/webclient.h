bc30f0e8cd3ca274c8da8dbef4b326b89a3bfd80
Sonia Shirwadkar <sonia.shirwadkar@gmail.com>
2017-01-26 01:28:27 -0600

    Delete webclient.h

--- hw1/webclient.h
+++
@@ -1,9 +0,0 @@
-#ifndef  _H_WEBCLIENT_H_
-#define  _H_WEBCLIENT_H_
-
-int getMD5(const char *url, char **md5);
-int getHeaderIndex(const char *ptr, int len);
-void removeSubstring(char *s,const char *toremove);
-int download(const char *urlToDownload , char *filename);
-
-#endif  //_H_WEBCLIENT_H_
